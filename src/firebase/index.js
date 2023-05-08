362c6585dedd50933d39a6fbf506329d651ea9f8
meriem <meriembenismail@insat.u-carthage.tn>
2023-05-08 22:05:35 +0100

    docs uploaded

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -12,7 +14,7 @@ const firebaseConfig = {
   storageBucket: "loan-management-ce9cc.appspot.com",
   messagingSenderId: "15943571482",
   appId: "1:15943571482:web:1c21b12b7ecb911b9959a3",
-  measurementId: "G-P31NR6GBZR"
+  measurementId: "G-P31NR6GBZR",
 };
 
 // Initialize Firebase
@@ -20,4 +22,6 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
--- src/pages/LoanApplication.jsx
+++ src/pages/LoanApplication.jsx
@@ -1,45 +1,171 @@
 import React, { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase/index";
 import { useNavigate } from "react-router";
+import "./loanApplication.css";
+import { doc, setDoc } from "firebase/firestore";
+import { db, auth, storage } from "../firebase/index";
+import { ref, uploadBytes } from "firebase/storage";
+
 const LoanApplication = () => {
   const [currentUser, setCurrentUser] = useState(null);
-
+  const [cin, setCin] = useState();
+  const [firstname, setFirstname] = useState();
+  const [lastname, setLastname] = useState();
+  const [address, setAddress] = useState();
+  const [job, setJob] = useState();
+  const [age, setAge] = useState();
+  const [salary, setSalary] = useState();
+  const [bankStatement, setBankStatement] = useState();
+  const [insuranceProof, setInsuranceProof] = useState();
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-
-        console.log(user);
+        //console.log(user);
         setCurrentUser(user);
       } else {
-        // User is signed out
-        // ...
         console.log("user is logged out");
       }
     });
   }, []);
+
+  const addLoanApp = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      cin: cin,
+      email: currentUser.email,
+      firstname: firstname,
+      lastname: lastname,
+      address: address,
+      job: job,
+      age: age,
+      salary: salary,
+    };
+    await setDoc(doc(db, "Client", cin), formData);
+    // store the uploaded documents
+    const bankStatementRef = ref(storage, `loan_files/bank-${lastname + cin}`);
+    uploadBytes(bankStatementRef, bankStatement).then(() => {
+      alert("bank statement uploaded");
+    });
+    const insuranceProofRef = ref(
+      storage,
+      `loan_files/insurance-${lastname + cin}`
+    );
+    uploadBytes(insuranceProofRef, insuranceProof).then(() => {
+      alert("insurance proof uploaded");
+    });
+    setSuccess(true);
+  };
   return (
     <div className="wrapper">
-      <div className="formContainer">
+      {success && (
+        <div
+          class="alert alert-success"
+          role="alert"
+          style={{ marginRight: "50px" }}
+        >
+          This is a success alert—check it out!
+        </div>
+      )}
+      <div className="formContainerLoan">
         <h3 style={{ color: "#1E1E1EB0" }}>Apply For A Loan</h3>
+
         <form>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label>cin</label>
             <input
-              type="email"
+              type="text"
               class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
+              placeholder="Enter cin"
+              onChange={(e) => setCin(e.target.value)}
             />
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
+          </div>
+          <div class="form-group">
+            <label>Firstname</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter Firstname"
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label>Lastname</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter Lastname"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label>Adress</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter Adress"
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label>Job</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter Job"
+              onChange={(e) => setJob(e.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label>Age</label>
+            <input
+              type="number"
+              class="form-control"
+              placeholder="Enter Age"
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label>Salary</label>
+            <input
+              type="number"
+              class="form-control"
+              placeholder="Enter Salary"
+              onChange={(e) => setSalary(e.target.value)}
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlFile1">Bank Statements</label>
+
+            <input
+              type="file"
+              class="form-control-file"
+              id="exampleFormControlFile1"
+              onChange={(e) => setBankStatement(e.target.files)}
+            />
+          </div>
+          <div class="form-group">
+            <label
+              for="exampleFormControlFile1"
+              onChange={(e) => setInsuranceProof(e.target.files)}
+            >
+              Insurance Proof
+            </label>
+
+            <input
+              type="file"
+              class="form-control-file"
+              id="exampleFormControlFile1"
+            />
           </div>
 
-          <button type="submit" class="btn btn-primary submit">
+          <button
+            type="submit"
+            class="btn btn-primary submit"
+            onClick={addLoanApp}
+          >
             Submit
           </button>
         </form>
